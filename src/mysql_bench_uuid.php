6996a4eb525b9410b5f596c123dd35cfff63ade6
Benjamin Michalski <benjamin.michalski@gmail.com>
2017-10-14 14:58:38 +0200

    Fix mysql_bench_uuid.php.

--- src/mysql_bench_uuid.php
+++ src/mysql_bench_uuid.php
@@ -2,10 +2,10 @@
 
 require __DIR__ . '/mysql_setup.php';
 
-$exec('CREATE TABLE uuid (uuid BINARY(16) PRIMARY KEY)');
+$exec('CREATE TABLE uuid (id BINARY(16) PRIMARY KEY)');
 
 $maxI = 10000;
 
 for ($i = 0; $i < $maxI; $i += 1) {
-    $execPrepared('INSERT INTO uuid (uuid) VALUES (:uuid)', [ 'uuid' => \Ramsey\Uuid\Uuid::uuid1()->getBytes() ], [ 'uuid' => \PDO::PARAM_LOB ]);
+    $execPrepared('INSERT INTO uuid (id) VALUES (:uuid)', [ 'uuid' => \Ramsey\Uuid\Uuid::uuid1()->getBytes() ], [ 'uuid' => \PDO::PARAM_LOB ]);
 }
