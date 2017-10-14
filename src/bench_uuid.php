83ebc803caff72960b51a9c87c23b56fc62a84f3
Benjamin Michalski <benjamin.michalski@gmail.com>
2017-10-14 12:53:49 +0200

    Fix bench_uuid.php.

--- src/bench_uuid.php
+++ src/bench_uuid.php
@@ -7,7 +7,5 @@ $exec('CREATE TABLE uuid (uuid BINARY(16) PRIMARY KEY)');
 $maxI = 10000;
 
 for ($i = 0; $i < $maxI; $i += 1) {
-    $uuid = \Ramsey\Uuid\Uuid::uuid1();
-
-    $execPrepared('INSERT INTO uuid (uuid) VALUES (:uuid)', [ 'uuid' => $uuid->getBytes() ], [ 'uuid' => \PDO::PARAM_LOB ]);
+    $execPrepared('INSERT INTO uuid (uuid) VALUES (:uuid)', [ 'uuid' => \Ramsey\Uuid\Uuid::uuid1()->getBytes() ], [ 'uuid' => \PDO::PARAM_LOB ]);
 }
